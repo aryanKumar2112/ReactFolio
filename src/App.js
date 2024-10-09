b8c21f8d8b856033ba7ade63454a582e74df38c6
Aryan <aryankumar211202@gmail.com>
2024-10-09 11:23:19 +0530

    Added props.children

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Item from './components/Item';
 import ItemDate  from './components/ItemDate';
+import Card from './components/Card';
 
 function App() {
   const response=[
@@ -26,13 +27,16 @@ function App() {
   ];
   return (
     <div>
-    <Item name={response[0].itemName}> hello ji // print by props.children</Item>
-    <ItemDate day={response[0].itemDate} month={response[0].itemMonth} year={response[0].itemYear}></ItemDate>
-    <Item name={response[1].itemName}></Item>
-    <ItemDate day={response[1].itemDate} month={response[1].itemMonth} year={response[1].itemYear}></ItemDate>
-    <Item name={response[2].itemName}></Item>
-    <ItemDate day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemYear}></ItemDate>
-    <div className="App">Hello Jee</div>
+      <Card>
+          <Item name={response[0].itemName}> hello ji // print by props.children</Item>
+          <ItemDate day={response[0].itemDate} month={response[0].itemMonth} year={response[0].itemYear}></ItemDate>
+          <Item name={response[1].itemName}></Item>
+          <ItemDate day={response[1].itemDate} month={response[1].itemMonth} year={response[1].itemYear}></ItemDate>
+          <Item name={response[2].itemName}></Item>
+          <ItemDate day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemYear}></ItemDate>
+          <div className="App">Hello Jee</div>
+      </Card>
+    
     </div>
   );
 }
